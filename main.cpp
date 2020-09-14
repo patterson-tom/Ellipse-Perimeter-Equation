683ee84c2fa36ccec7cb891e9bc40264c4b2b066
Thomas Patterson <thomasbpatterson@btinternet.com>
2020-09-15 00:50:39 +0100

    Refactored some equations for readability

--- main.cpp
+++ main.cpp
@@ -35,36 +35,55 @@ double percentage_accuracy(std::map<long double, long double> *ellipse_lengths, 
 
 //returns the derivate of the cost function with respect to weight j
 double derivative(std::map<long double, long double> *ellipse_lengths, std::vector<long double> *w, int j) {
+
     long double sum = 0;
     for (std::map<long double, long double>::iterator it = ellipse_lengths->begin(); it != ellipse_lengths->end(); it++) {
+
         long double a = it->first, b = 1;
+        long double h = hypothesis_function(a, b, w), diff = it->second - h;
+        
+        //each weight obviously has a different derivate so we switch on j to select the correct equation
         switch (j) {
-            case 0:
-                sum += -a * (it->second - hypothesis_function(a, b, w));
+            case 0: {
+                sum += -a * diff;
                 break;
-            case 1:
-                sum += -b * (it->second - hypothesis_function(a, b, w));
+            }
+            case 1: {
+                sum += -b * diff;
                 break;
-            case 2:
-                sum += ((0.05 * a * a) / (sqrt(w->at(2) * a * a + w->at(3) * a * b + w->at(4) * b * b))) * (it->second - hypothesis_function(a, b, w));
+            }
+            case 2: {
+                long double denominator = sqrt(w->at(2) * a * a + w->at(3) * a * b + w->at(4) * b * b);
+                sum += ((0.05 * a * a) / denominator) * diff;
                 break;
-            case 3:
-                sum += ((0.5 * a * b) / (sqrt(w->at(2) * a * a + w->at(3) * a * b + w->at(4) * b * b))) * (it->second - hypothesis_function(a, b, w));
+            }
+            case 3: {
+                long double denominator = sqrt(w->at(2) * a * a + w->at(3) * a * b + w->at(4) * b * b);
+                sum += ((0.5 * a * b) / denominator) * diff;
                 break;
-            case 4:
-                sum += ((0.5 * b * b) / (sqrt(w->at(2) * a * a + w->at(3) * a * b + w->at(4) * b * b))) * (it->second - hypothesis_function(a, b, w));
+            }
+            case 4: {
+                long double denominator = sqrt(w->at(2) * a * a + w->at(3) * a * b + w->at(4) * b * b);
+                sum += ((0.5 * b * b) / denominator) * diff;
                 break;
+            }
         }
     }
+
     return sum / ellipse_lengths->size();
 }
 
-void gradient_descent(std::map<long double, long double> *ellipse_lengths, std::vector<long double> *weights, int iterations = 1000, double learning_rate = 0.01, double learning_rate_modifier = 1) {
+void gradient_descent(std::map<long double, long double> *ellipse_lengths, std::vector<long double> *weights, 
+                        int iterations = 1000, double learning_rate = 0.01, double learning_rate_modifier = 1) {
+
+    //loop through each iteration and within each iteration loop through and update each weight                        
     for (int i = 0; i < iterations; i++) {
         for (int j = 0; j < 5; j++) {
+
             //max() used as if weights are negative then we could end up trying to sqrt a negative
             weights->at(j) = std::max(0.L, weights->at(j) - learning_rate * derivative(ellipse_lengths, weights, j));
         }
+
         //hopefully helps to avoid local minima
         learning_rate *= learning_rate_modifier;
     }
@@ -114,7 +133,7 @@ int main() {
     for (int i = 0; i < 5; i++) {
         std::cout << weights.at(i) << ", ";
     }
-    std::cout << "\n Cost function value: " << cost_function(&ellipse_lengths, &weights) << std::endl;
+    std::cout << "\nCost function value: " << cost_function(&ellipse_lengths, &weights) << std::endl;
     std::cout << "Mean percentage accuracy: " <<percentage_accuracy(&ellipse_lengths, &weights) << std::endl;
 
     return 0;
